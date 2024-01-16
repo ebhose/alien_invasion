4dd05cb24855cf43ff5adc1b20e5582eb4a3b192
ebhose <ebhose@localdomain.com>
2024-01-16 23:26:13 +0100

    Add file ship.py and ship image.
    The file ship.py includes the class Ship to manage the ship.
    
    A ship image has also been added in the images directory.
    
    File alien_invasion.py has been update to draw the ship to the screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@
 import pygame
 import sys
 from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -18,6 +20,8 @@ class AlienInvasion:
                 (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -28,6 +32,7 @@ class AlienInvasion:
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
