a0dd7f0f16b3ed64a841a4d2275e5991fb07cb26
ebhose <ebhose@localdomain.com>
2024-01-17 00:02:04 +0100

    Add continuous left and right movement to the ship.
    Add movement to the function _check_events, so that the ship can be moved
    on the screen.
    
    Add function update the file ship.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,24 +20,38 @@ class AlienInvasion:
                 (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        # Set the background color.
+        # self.bg_color = (230, 230, 230)
         self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
-
             self.clock.tick(60)
 
     def _check_events(self):
-        """Respons to keypresses and mouse events."""
+        """Respond to  keypresses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
     def _update_screen(self):
-        "Update images on the screen, and flip to the new screen."""
+        """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
 
