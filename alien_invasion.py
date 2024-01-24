4ef44dc646d1ecf257a9fd74579538ddfab9952f
ebhose <ebhose@localdomain.com>
2024-01-24 18:46:38 +0100

    Add scoring capabilities
    Modify class GameStats in file game_stats.py to keep the scoring.
    
    Add file scoreboard.py to display the score on the Surface.
    
    Modify function initialize_dynamic_settings in file settings.py to add
    points per alien hit.
    
    Modify function _check_bullet_alien_collisions in file_alien_invasion.py
    to update the score when alien is hit.

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -26,8 +27,10 @@ class AlienInvasion:
                 (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        # Create an instance to store game statistics.
+        # Create an instance to store game statistics,
+        #  and create a scoreboard.
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -130,6 +133,10 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(
                 self.bullets, self.aliens, True, True)
 
+        if collisions:
+            self.stats.score += self.settings.alien_points
+            self.sb.prep_score()
+
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
@@ -223,6 +230,9 @@ class AlienInvasion:
 
         self.ship.blitme()
         self.aliens.draw(self.screen)
+
+        # Draw the score information.
+        self.sb.show_score()
 
         # Draw the play button if the game is inactive.
         if not self.game_active:
